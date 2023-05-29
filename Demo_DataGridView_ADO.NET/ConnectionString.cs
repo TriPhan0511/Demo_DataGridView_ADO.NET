82299c3eb3ecf1dffa443aec5287fa83be6181bc
Tri Phan <phanphutri@gmail.com>
2023-05-29 15:18:50 +0700

    First commit

---
+++ Demo_DataGridView_ADO.NET/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_DataGridView_ADO.NET
+{
+    class ConnectionString
+    {
+        public static string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=AccessingDataApp_DB;Integrated Security=True";
+    }
+}
